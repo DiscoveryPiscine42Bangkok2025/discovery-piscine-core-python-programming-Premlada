8587be4880c72c029d0de9e1c2064b4305dfc795
Premlada <142295065+Premlada@users.noreply.github.com>
2026-04-25 13:50:10 +0700

    Delete cell02/cell02 directory

--- cell02/cell02/ex01/isneg.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter a number: "))
-if n < 0:
-    print("This number is negative.")
-elif n > 0:
-    print("This number is positive.")
-else:
-    print("This number is both positive and negative.")
--- cell02/cell02/ex03/mult.py
+++
@@ -1,9 +0,0 @@
-first = int(input("Enter the first number = "))
-second = int(input("Enter the second number ="))
-result = first * second
-if result < 0:
-    print("The result is negative.")
-elif result > 0:
-    print("The result is positive.")
-else:
-    print("The result is positive and negative.")
