505559e2fbce9360ed66a7a35c4c40b451edb281
Premlada <142295065+Premlada@users.noreply.github.com>
2026-04-28 09:42:11 +0700

    Delete discovery_piscine/cell03 directory

--- discovery_piscine/cell03/ex02/i_got_that.py
+++
@@ -1,5 +0,0 @@
-w = str(input("What you gotta say? :"))
-while True:
-    w = input("What you gotta say? :")
-    if w == "STOP":
-       break
--- discovery_piscine/cell03/ex03/advanced_mult.py
+++
@@ -1,9 +0,0 @@
-n = 0
-while n<= 10:
-    m = 0
-    print(f"Table de {n}: ", end=" ")
-    while m <= 10:
-        print(n * m, end=" ")
-        m += 1
-        print()
-        n += 1
